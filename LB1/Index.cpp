83b6d24c78c3755bffd3a9a4fa5e1a1a1eb0352a
Silver Pupa <ivan.soloviov@nure.ua>
2022-02-22 13:42:00 +0200

    Добавьте файлы проекта.

---
+++ LB1/Index.cpp
@@ -0,0 +1,99 @@
+#include <windows.h>; // підключення бібліотеки з функціями API
+// Глобальні змінні:
+HINSTANCE hInst; //Дескриптор програми
+LPCTSTR szWindowClass = "QWERTY";
+LPCTSTR szTitle = "ПЕРША ПРОГРАМА";
+// Попередній опис функцій
+ATOM MyRegisterClass(HINSTANCE hInstance);
+BOOL InitInstance(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// Основна програма
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	MSG msg;
+	// Реєстрація класу вікна
+	MyRegisterClass(hInstance);
+	// Створення вікна програми
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+	// Цикл обробки повідомлень
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW; //стиль вікна
+	wcex.lpfnWndProc = (WNDPROC)WndProc; //віконна процедура
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance; //дескриптор програми
+	wcex.hIcon = LoadIcon(NULL, IDI_HAND); //визначення іконки
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW); //визначення курсору
+	wcex.hbrBackground = GetSysColorBrush(COLOR_WINDOW + 1); //установка фону
+	wcex.lpszMenuName = NULL; //визначення меню
+	wcex.lpszClassName = szWindowClass; //ім’я класу
+	wcex.hIconSm = NULL;
+	return RegisterClassEx(&wcex); //реєстрація класу вікна
+}
+// FUNCTION: InitInstance (HANDLE, int)
+// Створює вікно програми і зберігає дескриптор програми в змінній hInst
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	HWND hWnd;
+	hInst = hInstance; //зберігає дескриптор додатка в змінній hInst
+	hWnd = CreateWindow(szWindowClass, // ім’я класу вікна
+		szTitle, // назва програми
+		WS_OVERLAPPEDWINDOW, // стиль вікна
+		CW_USEDEFAULT, // положення по Х
+		CW_USEDEFAULT, // положення по Y
+		CW_USEDEFAULT, // розмір по Х
+		CW_USEDEFAULT, // розмір по Y
+		NULL, // дескриптор батьківського вікна
+		NULL, // дескриптор меню вікна
+		hInstance, // дескриптор програми
+		NULL); // параметри створення.
+	if (!hWnd) //Якщо вікно не творилось, функція повертає FALSE
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow); //Показати вікно
+	UpdateWindow(hWnd); //Оновити вікно
+	return TRUE;
+}
+// FUNCTION: WndProc (HWND, unsigned, WORD, LONG)
+// Віконна процедура. Приймає і обробляє всі повідомлення, що приходять в додаток
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+	RECT rt;
+	switch (message)
+	{
+	case WM_CREATE: //Повідомлення приходить при створенні вікна
+		break;
+	case WM_PAINT: //Перемалювати вікно
+		hdc = BeginPaint(hWnd, &ps); //Почати графічний вивід
+		GetClientRect(hWnd, &rt); //Область вікна для малювання
+		DrawText(hdc, "Hellow world!", -1, &rt, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+		EndPaint(hWnd, &ps); //Закінчити графічний вивід
+		break;
+	case WM_DESTROY: //Завершення роботи
+		PostQuitMessage(0);
+		break;
+	default:
+		//Обробка повідомлень, які не оброблені користувачем
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
